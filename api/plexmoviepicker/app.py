9e2a71f882a0ab2d30ffa97d3e8e6e2c54776a5e
Damien Deville <damien@ddeville.me>
2023-05-16 19:16:24 -0700

    check env var

--- api/plexmoviepicker/app.py
+++ api/plexmoviepicker/app.py
@@ -1,9 +1,10 @@
+import os
 import time
 from flask import Flask
 
 app = Flask("plexmoviebuilder")
 
 
-@app.route('/api/time')
+@app.route("/api/time")
 def get_current_time():
-    return {'time': time.time()}
+    return {"time": time.time(), "location": os.environ.get("PLEX_LOCATION", "env not set")}
