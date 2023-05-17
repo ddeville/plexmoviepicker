f97f59c50d7b939d5cc835d6b0476704084ee1e7
Damien Deville <damien@ddeville.me>
2023-05-16 23:40:45 -0700

    pull metadata

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [metadata, setMetadata] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/metadata").then(res => res.json()).then(data => {
+      console.log(data);
+      setMetadata(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Metadata {metadata}.</p>
       </header>
     </div>
   );
